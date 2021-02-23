64d681ffd728418b5714aa4044c25b69269396f5
hdiedrich127 <60328397+hdiedrich127@users.noreply.github.com>
2021-02-23 16:38:15 -0600

    Update Contents.swift

--- Diedrich_ClockRecipe.playground/Contents.swift
+++ Diedrich_ClockRecipe.playground/Contents.swift
@@ -8,6 +8,6 @@ Ingredients:
 
 Directions:
 1. When the program starts the clock will trigger and start a count
-2. Add one to seconds until it reaches 60 then reset
+2. Add 1 to seconds until it reaches 60 then reset
 3. When seconds resets add 1 to minutes
 4. When minutes equals 60 add 1 to hours and reset minutes to 0
