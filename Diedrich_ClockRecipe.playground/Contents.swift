e846e0ffe42ce3ea5ecc925d7e60574997717fa0
hdiedrich127 <60328397+hdiedrich127@users.noreply.github.com>
2021-02-23 16:37:05 -0600

    Clock Recipe

---
+++ Diedrich_ClockRecipe.playground/Contents.swift
@@ -0,0 +1,13 @@
+Clock Recipe
+
+Ingredients:
+1. Apple's timer class
+2. seconds variable
+3. minutes variable
+4. hours variable
+
+Directions:
+1. When the program starts the clock will trigger and start a count
+2. Add one to seconds until it reaches 60 then reset
+3. When seconds resets add 1 to minutes
+4. When minutes equals 60 add 1 to hours and reset minutes to 0
